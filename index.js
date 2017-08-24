dec8c71c19b9e8437b5945d4dccf60023dff68c5
wang_jingbao <com.ri.life@gmail.com>
2017-08-24 18:23:26 +0800

    Add a custom file name setting

--- index.js
+++ index.js
@@ -9,45 +9,75 @@ function SimpleHtmlPrecompiler (staticDir, paths, options) {
   this.options = options || {}
 }
 
-SimpleHtmlPrecompiler.prototype.apply = function (compiler) {
+SimpleHtmlPrecompiler.prototype.task = function () {
   var self = this
-  compiler.plugin('after-emit', function (compilation, done) {
-    Promise.all(
-      self.paths.map(function (outputPath) {
-        return new Promise(function (resolve, reject) {
-          compileToHTML(self.staticDir, outputPath, self.options, function (prerenderedHTML) {
-            if (self.options.postProcessHtml) {
-              prerenderedHTML = self.options.postProcessHtml({
-                html: prerenderedHTML,
-                route: outputPath
-              })
-            }
-            var folder = Path.join(self.options.outputDir || self.staticDir, outputPath)
-            mkdirp(folder, function (error) {
-              if (error) {
-                return reject('Folder could not be created: ' + folder + '\n' + error)
-              }
-              var file = Path.join(folder, 'index.html')
-              FS.writeFile(
-                file,
-                prerenderedHTML,
-                function (error) {
-                  if (error) {
-                    return reject('Could not write file: ' + file + '\n' + error)
-                  }
-                  resolve()
-                }
-              )
+  return Promise.all(
+    self.paths.map(function (outputPath) {
+      if (typeof outputPath === 'string') {
+        outputPath = {
+          route: outputPath
+        }
+      }
+      return new Promise(function (resolve, reject) {
+        compileToHTML(self.staticDir, outputPath.route, self.options, function (prerenderedHTML) {
+          if (outputPath.title) {
+            prerenderedHTML = prerenderedHTML.replace(
+              /<title>[^<]*<\/title>/i,
+              '<title>' + outputPath.title + '</title>'
+            )
+          }
+          if (self.options.postProcessHtml) {
+            prerenderedHTML = self.options.postProcessHtml({
+              html: prerenderedHTML,
+              route: outputPath.route
             })
+          }
+          var folder = Path.join(
+            self.options.outputDir || self.staticDir,
+            outputPath.filename ? Path.dirname(outputPath.filename) : outputPath.route
+          )
+          mkdirp(folder, function (error) {
+            if (error) {
+              return reject('Folder could not be created: ' + folder + '\n' + error)
+            }
+            var file = Path.join(folder, Path.basename(outputPath.filename || 'index.html'))
+            FS.writeFile(
+              file,
+              prerenderedHTML,
+              function (error) {
+                if (error) {
+                  return reject('Could not write file: ' + file + '\n' + error)
+                }
+                resolve()
+                console.log('Write file successfully:', file)
+              }
+            )
           })
         })
       })
-    )
+    })
+  )
+}
+
+SimpleHtmlPrecompiler.prototype.apply = function (compiler) {
+  var self = this
+  compiler.plugin('after-emit', function (compilation, done) {
+    self.task()
     .then(function () { done() })
     .catch(function (error) {
       // setTimeout prevents the Promise from swallowing the throw
       setTimeout(function () { throw error })
     })
+  })
+}
+
+SimpleHtmlPrecompiler.prototype.build = function () {
+  this.task()
+  .then(function () {
+    console.log('render done...')
+  })
+  .catch(function (error) {
+    setTimeout(function () { throw error })
   })
 }
 
